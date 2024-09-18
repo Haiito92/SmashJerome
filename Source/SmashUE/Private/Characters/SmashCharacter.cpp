14b7c491fcdf8abafbfd083aaa1b76d1a007964e
Antoine Hanna <hannaantoinebusiness@gmail.com>
2024-09-18 14:50:57 +0200

    [ADD] SmashCharacter StateMachine/State/StateIdle/StateWalk

--- Source/SmashUE/Private/Characters/SmashCharacter.cpp
+++ Source/SmashUE/Private/Characters/SmashCharacter.cpp
@@ -3,6 +3,8 @@
 
 #include "Characters/SmashCharacter.h"
 
+#include "Characters/SmashCharacterStateMachine.h"
+
 #pragma region Unreal Default
 // Sets default values
 ASmashCharacter::ASmashCharacter()
@@ -15,7 +17,9 @@ ASmashCharacter::ASmashCharacter()
 void ASmashCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	CreateStateMachine();
+
+	InitStateMachine();
 }
 
 // Called every frame
@@ -50,4 +54,19 @@ void ASmashCharacter::RotateMeshUsingOrient() const
 	GetMesh()->SetRelativeRotation(Rotation);
 	
 }
+#pragma endregion 
+
+#pragma region State Machine
+
+void ASmashCharacter::CreateStateMachine()
+{
+	StateMachine = NewObject<USmashCharacterStateMachine>(this);
+}
+
+void ASmashCharacter::InitStateMachine()
+{
+	if(StateMachine == nullptr) return;
+	StateMachine->Init(this);
+}
+
 #pragma endregion 
--- Source/SmashUE/Public/Characters/SmashCharacter.h
+++ Source/SmashUE/Public/Characters/SmashCharacter.h
@@ -6,6 +6,8 @@
 #include "GameFramework/Character.h"
 #include "SmashCharacter.generated.h"
 
+class USmashCharacterStateMachine;
+
 UCLASS()
 class SMASHUE_API ASmashCharacter : public ACharacter
 {
@@ -40,5 +42,16 @@ protected:
 	float OrientX = 1.0f;
 
 	void RotateMeshUsingOrient() const;
+#pragma endregion
+
+#pragma region State Machine
+	
+public:
+	void CreateStateMachine();
+
+	void InitStateMachine();
+
+protected:
+	TObjectPtr<USmashCharacterStateMachine> StateMachine;
 #pragma endregion 
 };
